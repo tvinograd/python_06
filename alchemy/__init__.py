7eea42f103b83fd3bbe9ff2c948be394fc7af572
Tatiana Vinogradova <tatiana.vinogradova@tum.de>
2026-03-29 21:29:17 +0200

    switched from # noqa: F401 to __all__

--- alchemy/__init__.py
+++ alchemy/__init__.py
@@ -1,4 +1,5 @@
 __version__ = "1.0.0"
 __author__ = "Master Pythonicus"
+__all__ = ["create_fire", "create_water"]
 
-from .elements import create_fire, create_water  # noqa: F401
+from .elements import create_fire, create_water
--- alchemy/grimoire/__init__.py
+++ alchemy/grimoire/__init__.py
@@ -1,2 +1,4 @@
-from .spellbook import record_spell  # noqa: F401
-from .validator import validate_ingredients  # noqa: F401
+__all__ = ["record_spell", "validate_ingredients"]
+
+from .spellbook import record_spell
+from .validator import validate_ingredients
